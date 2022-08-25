db1a975d1ed9d74ac9bf461627b724267cedf450
Ivan Lazarov <ivan@betixon.com>
2022-08-25 19:48:01 +0300

    Thank you card shown on submit of the form ready

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const button = document.querySelector(".submit-button");
 const ratesForm = document.querySelector(".rates-form");
+const ratings = document.getElementsByName("ratings");
+const mainCard = document.querySelector("#main-card");
+const hiddenCard = document.querySelector(".card");
+const scoreSpan = document.querySelector("#score");
+
+const toggleHidden = () =>{
+    mainCard.classList.toggle("hidden");
+    hiddenCard.classList.toggle("hidden");
+}
 
 ratesForm.addEventListener("submit", (event)=>{
     event.preventDefault();
-    console.log(ratesForm.innerHTML);
-
+    let selected = Array.from(ratings).find(rating => rating.checked);
+    scoreSpan.textContent = `You selected ${selected.value} out of 5`;
+    toggleHidden();
     return ()=> ratesForm.submit();
 });
 
 
-let spanText = `You selected ${3} out of 5`;
